268d20c19b9e85d5e7f100e1d429d85e534a8348
xristina <xristina@mist.io>
2017-01-30 04:00:25 +0200

    features element and prepare for testimonials

---
+++ data/features.js
@@ -0,0 +1,26 @@
+var FEATURES = [{
+    "title": "Single panel",
+    "icon": "hardware:laptop-mac",
+    "description": "Control public & private clouds, containers, bare metal servers, more."
+  },{
+    "title": "Role Base Access",
+    "icon": "icons:supervisor-account",
+    "description": "Fine grain controls for delegating access to team members."
+  },{
+    "title": "Cost Visibility.",
+    "icon": "icons:visibility",
+    "description": "Cost and usage reporting across all your infrastructure.",
+    "new": true
+  },{
+    "title": "Simplify your workflows",
+    "icon": "icons:cached",
+    "description": "Orchestrate repeatable deployments, automate common responses."
+  },{
+    "title": "Secure",
+    "icon": "hardware:security",
+    "description": "Enforce your policies consistently, across any computing platform."
+  },{
+    "title": "No migration",
+    "icon": "icons:cloud-done",
+    "description": "Re-architecting infrastructure not required. Get started fast."
+ }]
