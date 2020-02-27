222914dd2d8182bf8fdf3c2b8f3f65e0cac36880
xristina <xristina@mist.io>
2020-02-27 21:01:48 +0200

    Smaller providers

--- src/shared-styles.js
+++ src/shared-styles.js
@@ -951,7 +951,7 @@ $_documentContainer.innerHTML = `<dom-module id="shared-styles">
         
         .provider img {
             opacity: .4;
-            max-width: 180px;
+            max-width: 130px;
             filter: grayscale(100%);
             -webkit-filter: grayscale(100%);
             -webkit-transition: all .2s ease-in-out;
