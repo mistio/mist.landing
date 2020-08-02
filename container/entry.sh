e40fb99f78db2a38e8fc9545bb26d3528e5a69c5
Dimitris Moraitis <dimo@mist.io>
2020-08-03 01:59:51 +0300

    Do not always compile landing-dev js files

--- container/entry.sh
+++ container/entry.sh
@@ -7,6 +7,6 @@ if ! diff -q package.json /package.json; then
     npm install
 fi
 
-polymer serve --npm -H 0.0.0.0 -p 8000 --compile=always &
+polymer serve --npm -H 0.0.0.0 -p 8000 &
 
 exec nginx
